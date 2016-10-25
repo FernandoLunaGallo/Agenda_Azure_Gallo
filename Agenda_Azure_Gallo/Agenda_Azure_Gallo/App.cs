e589b200eb867a53c1acdc060622c3ac06645818
FernandoGalloLuna <FernandoGalloLuna@FERNANDOLUNA>
2016-10-24 19:43:08 -0500

    Agregar archivos de proyecto.

---
+++ Agenda_Azure_Gallo/Agenda_Azure_Gallo/App.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.WindowsAzure.MobileServices;
+using Xamarin.Forms;
+using Xamarin.Forms;
+
+namespace Agenda_Azure_Gallo
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+
+            MobileServiceClient client;
+            IMobileServiceTable<agendagallo> tabla;
+            client = new MobileServiceClient(Conexion.ApplicationURL);
+            tabla = client.GetTable<agendagallo>();
+            Label titulo = new Label()
+            {
+                Text = "Inserta datos"
+            };
+
+            Entry nombre = new Entry();
+            Entry apellido = new Entry();
+            Entry telefono = new Entry();
+
+
+            Button enviar = new Button()
+            {
+                Text = "enviar datos"
+            };
+            enviar.Clicked += async (sender, args) =>
+            {
+                var datos = new agendagallo { Name = nombre.Text, Ape = apellido.Text, Tel = telefono.Text };
+                if (datos.Id == null)
+                {
+                    await tabla.InsertAsync(datos);
+                }
+                else
+                {
+                    await tabla.UndeleteAsync(datos);
+                }
+            };
+            var layout = new StackLayout();
+            layout.Children.Add(titulo);
+            layout.Children.Add(nombre);
+            layout.Children.Add(apellido);
+            layout.Children.Add(telefono);
+            layout.Children.Add(enviar);
+
+            MainPage = new ContentPage()
+            {
+                Content = layout
+            };
+
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ Agenda_Azure_Gallo/Agenda_Azure_Gallo/Tabla.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.MobileServices;
+using Newtonsoft.Json;
+namespace Agenda_Azure_Gallo
+{
+    public class agendagallo
+    {
+        string id;
+        string name;
+        string apellido;
+        string telefono;
+
+        [JsonProperty(PropertyName = "id")]
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        [JsonProperty(PropertyName = "Nombre")]
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        [JsonProperty(PropertyName = "Apellido")]
+        public string Ape
+        {
+            get { return apellido; }
+            set { apellido = value; }
+        }
+        [JsonProperty(PropertyName = "Telefono")]
+        public string Tel
+        {
+            get { return telefono; }
+            set { telefono = value; }
+        }
+
+        [Version]
+        public string Version { get; set; }
+    }
+}
